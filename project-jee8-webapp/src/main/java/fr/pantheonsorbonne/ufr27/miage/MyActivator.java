161d9a0ec40a4a293749de783523e9283f864310
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-11-08 16:13:26 +0100

    initial version

---
+++ project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/MyActivator.java
@@ -0,0 +1,9 @@
+package fr.pantheonsorbonne.ufr27.miage;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MyActivator extends Application {
+
+}

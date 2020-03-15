426538cf2bab1de773738685cb1aa9b46fb52bb2
Shiny <sshn@pm.me>
2020-03-15 16:19:19 +0100

    Update

---
+++ project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/ejb/InitService.java
@@ -0,0 +1,5 @@
+package fr.pantheonsorbonne.ufr27.miage.ejb;
+
+public interface InitService {
+	public void init();
+}
--- project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/resource/InitEndpoint.java
+++ project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/resource/InitEndpoint.java
@@ -1,17 +1,19 @@
 package fr.pantheonsorbonne.ufr27.miage.resource;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+
+import fr.pantheonsorbonne.ufr27.miage.ejb.InitService;
 
 @Path("/init")
 public class InitEndpoint {
 	
 	@Inject
-	EntityManager em;
+	InitService service;
 	
 	@POST
 	public void init() {
+		service.init();
 	}
 }
